5f195781398257befd7c19bc173f0dde0e3c3064
Аня <@LAPTOP-5IRPDSG2>
2024-03-08 21:10:50 +0700

    Добавьте файлы проекта.

---
+++ lab4IM/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace lab4IM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.kletka = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(122, 85);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поехали!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // kletka
+            // 
+            this.kletka.BackColor = System.Drawing.Color.DarkGray;
+            this.kletka.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.kletka.Location = new System.Drawing.Point(98, 245);
+            this.kletka.Name = "kletka";
+            this.kletka.Size = new System.Drawing.Size(808, 586);
+            this.kletka.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(711, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 85);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Чистка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(417, 85);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 85);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Стоп!!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 885);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.kletka);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel kletka;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab4IM/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4IM
+{
+    public partial class Form1 : Form
+    {
+
+        int N = 20, M = 35;
+        int i, j;
+
+        int[,] matrix;
+        MatrixGrid grid;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            matrix = new int[M, N];//первое значение количество столбцов второе строк
+
+            grid = new MatrixGrid()
+            {
+                Parent = kletka,
+                Dock = DockStyle.Fill,
+                GridSize = new Size(M, N),
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+
+            grid.CellNeeded += grid_CellNeeded;
+            grid.CellClick += grid_CellClick;
+        }
+        void grid_CellClick(object sender, MatrixGrid.CellClickEventArgs e)
+        {
+            matrix[e.Cell.X, e.Cell.Y] = 1 - matrix[e.Cell.X, e.Cell.Y];
+        }
+
+        void grid_CellNeeded(object sender, MatrixGrid.CellNeededEventArgs e)
+        {
+            e.BackColor = matrix[e.Cell.X, e.Cell.Y] > 0 ? Color.Blue : Color.White;
+        }
+
+        private void button2_Click(object sender, EventArgs e)//кнопка очистки, если что чистка рисунка задерживается(может это только на ноутбуке) нужно ждать
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        Random rand = new Random();
+
+        private void btRandom_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, 0] = rand.Next(0, 2);//заполнение от 0 до 1 //i-столбец, 0-строка, реализацию брала в интернете, там отдельным классом сделали matrixGrid, у них строки со столбцами перепутаны
+            }
+
+            Invalidate(true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            grid.Refresh();
+
+            for (int j = 0; j < N; j++)
+            {
+                for (int i = 0; i < M; i++)
+                {
+                    
+                    Rule(i, j);
+                }
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        public void Rule(int ii, int jj)
+        {
+            int neighbours = Neighbours(ii, jj);
+
+            if (neighbours < 3 && neighbours > 1)
+            {
+                matrix[ii, jj] = 0;
+            }
+            else
+                if (neighbours == 5 && (jj + 1) < N && matrix[ii, jj + 1] == 1)
+            {
+                matrix[ii, jj] = 0;
+            }
+            else
+                if (neighbours == 1)
+            {
+                matrix[ii, jj] = 1;
+            }
+            else
+                if (neighbours > 6)
+            {
+                matrix[ii, jj] = 0;
+            }
+            else
+                if (neighbours == 7 && (ii - 1) > 0 && (ii + 1) < M && matrix[ii + 1, jj] == 1 && matrix[ii - 1, jj] == 1)
+            {
+                matrix[ii - 1, jj] = 0;//умирает сосед снизу
+            }
+
+        }
+
+        public int Neighbours(int iii, int jjj)//окрестность на 11 соседей
+        {
+            int neighbours = 0;
+
+            for (int j = jjj - 1; j < jjj + 2; j++)
+            {
+                for (int i = iii - 1; i < iii + 3; i++)
+                {
+                    if (i >= 0 && j >= 0 && i < M && j < N && (i != iii || j != jjj))
+                    {
+                        if (matrix[i, j] > 0)
+                            neighbours++;
+                    }
+                }
+            }
+            return neighbours;
+        }
+
+
+
+    }
+}
